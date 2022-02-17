c08dd5f014832d0f14922d1de6cb820f94240b7c
javierpozzi <javierpozzi@gmail.com>
2022-02-17 18:00:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gentrees NFT
+
+Gentrees is a 1k procedurally generated collection of art trees!
+
+It uses the ERC721 standard with lazy minting, and IPFS as data storage.
---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "./scripts/tasks";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    polygon: {
+      url: process.env.POLYGON_URL!,
+      accounts: [`0x${process.env.PRIVATE_KEY!}`],
+    },
+    mumbai: {
+      url: process.env.MUMBAI_URL!,
+      accounts: [`0x${process.env.PRIVATE_KEY!}`],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY!,
+    },
+  },
+};
+
+export default config;
